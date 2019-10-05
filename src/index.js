6e72ac8af47f9d1153e59f5fdb39d8cad30244ec
Dmitry Radkovets <dmitry.radkovets@gmail.com>
2019-10-05 13:25:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let array = expr.split('');
+    let arrMorse = [];
+    let space = 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] == '*') {
+            space++;        
+        }
+        if (space == 10) {
+            arrMorse.push(' ');
+            space = 0;
+        }
+        if (i % 10 == 0 && i != 0 && i != array.length - 1) {
+            arrMorse.push(',');
+        }
+        if (array[i] == '1') {
+            arrMorse.push(array[i]);
+        }
+        if (arrMorse[arrMorse.length - 1] == '1' && arrMorse[arrMorse.length - 2] == '1') {
+            arrMorse[arrMorse.length - 2] = '-';
+            arrMorse.splice(-1,1);
+        }
+        if (array[i] == '0' && arrMorse[arrMorse.length - 1] == '1') {
+            arrMorse[arrMorse.length - 1] = '.';
+        }
+
+    }
+
+    let arrResult = arrMorse.join('').split(',');
+    let message = '';
+
+    for (let i = 0; i < arrResult.length; i++) {
+
+        if (MORSE_TABLE[arrResult[i]]) {
+            message += MORSE_TABLE[arrResult[i]];
+        }
+        if (arrResult[i] == ' '){
+            message += ' ';
+        }
+
+    }
+    
+    return message;
 }
 
 module.exports = {
